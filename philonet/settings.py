f918cb23e7c478c6c3e0e74b98bb9260f2867a92
Jon Besga <jonbesga@users.noreply.github.com>
2026-01-28 16:10:27 +0000

    Resolve password validator conflict

--- philonet/settings.py
+++ philonet/settings.py
@@ -112,14 +112,8 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
         'OPTIONS': {
-            'min_length': 12,
-        }
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+            'min_length': 6,
+        },
     },
     {
         'NAME': 'questions.validators.ComplexityPasswordValidator',
